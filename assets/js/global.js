b39c4444a865fc7dcfd25620206d26c3a1938ef1
Diego Lorenzo <diego@diegolorenzo.com>
2013-10-09 00:46:54 -0400

    Creating example of slide panels to post as a GH pages site.

---
+++ assets/js/global.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+
+	document.addEventListener("touchstart", function(){}, true);
+
+
+	var page = $('#page');
+	
+	$('#utility-nav').on('click', 'a', function(event) {
+		event.preventDefault();
+
+		var panel = $(this).data('panel');
+
+		if (panel == 'main-nav') {
+			page.removeClass('is-showing-settings');
+			page.addClass('is-showing-main-nav');
+		} else if (panel === 'settings') {
+			page.removeClass('is-showing-main-nav');
+			page.addClass('is-showing-settings');
+		}
+	});
+
+	$('#main-nav').on('click', '.btn-close', function() {
+		page.removeClass('is-showing-main-nav');
+	});
+
+	$('#settings').on('click', '.btn-close', function() {
+		page.removeClass('is-showing-settings');
+	});
+});
